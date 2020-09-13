99d78238dc4ea6bb22f178cd75bec5de9300d701
sanjay.ujjainkar <sanjay.ujjainkar@globant.com>
2020-09-13 21:57:22 +0530

    Initial commit, Skeleton application for Jet airways.

---
+++ app/src/main/java/com/pof/jettravel/data/model/Medium.java
@@ -0,0 +1,12 @@
+package com.pof.jettravel.data.model;
+
+import java.util.Date;
+
+public class Medium{
+    public String id;
+    public String blogId;
+    public Date createdAt;
+    public String image;
+    public String title;
+    public String url;
+}
